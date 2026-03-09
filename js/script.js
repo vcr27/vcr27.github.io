7a3f58e643652a9f641f25fb2a2112cb32bc8dc9
vcr27 <162660409+vcr27@users.noreply.github.com>
2026-03-09 17:24:13 -0400

    please god
    please god

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-function toggleDark(){
+function reveal(){
 
-document.body.classList.toggle("dark")
+let reveals = document.querySelectorAll(".reveal");
+
+for(let i=0;i<reveals.length;i++){
+
+let windowHeight = window.innerHeight;
+
+let elementTop = reveals[i].getBoundingClientRect().top;
+
+let elementVisible = 100;
+
+if(elementTop < windowHeight - elementVisible){
+
+reveals[i].classList.add("active");
 
 }
+
+}
+
+}
+
+window.addEventListener("scroll", reveal);
